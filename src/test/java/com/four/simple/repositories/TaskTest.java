c8c46d3ed5371586d7409196118413d32d028025
Satya Yoganand <satyaadd22@gmail.com>
2023-04-16 15:24:24 -0500

    Updated CRUD Operations for TaskTest

--- src/test/java/com/four/simple/repositories/TaskTest.java
+++ src/test/java/com/four/simple/repositories/TaskTest.java
@@ -3,12 +3,15 @@ package com.four.simple.repositories;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.four.simple.models.Task;
+
+
 
 
 /*
@@ -34,6 +37,26 @@ public class TaskTest {
 
         List<Task> tasksL2 = taskRepo.findByListId(2L);
         assertEquals(4, tasksL2.size());
+
+         // test READ
+         Task task = taskRepo.findById(1L).get();
+         assertEquals(true, task != null);
+         assertEquals(1L, task.getId());
+         assertEquals("Schedule an appointment with S Supermarket", task.getDescription());
+
+          // test UPDATE
+        task = taskRepo.findById(1L).get();
+        task.setDescription("Testing Task");
+        taskRepo.save(task);
+
+        Task updated = taskRepo.findById(1L).get();
+        assertEquals("Testing Task", updated.getDescription());
+
+        // test DELETE
+        taskRepo.deleteById(1L);
+        Optional<Task> deleted = taskRepo.findById(1L);
+        assertEquals(false, deleted.isPresent());
+
     }
 }
 
