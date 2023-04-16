929e0be2e5405c07993e7893fa570c970049c300
Khant Tayza <khant.tz@proton.me>
2023-04-16 16:05:41 -0500

    add a CRUD test for TaskRepository

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,8 +9,8 @@ INSERT INTO workspaces (id, name, user_id)
 -- Lists
 INSERT INTO lists (id, name, workspace_id)
     VALUES
-        (1, 'Marketing To-dos', 100),
-        (2, 'Ideas for Marketing Projects', 100);
+        (100, 'Marketing To-dos', 100),
+        (200, 'Ideas for Marketing Projects', 100);
 
 -- Statuses
 INSERT INTO statuses (id, description, list_id)
@@ -24,13 +24,13 @@ INSERT INTO statuses (id, description, list_id)
 -- Tasks
 INSERT INTO tasks (id, description, list_id, status_id)
     VALUES
-        (1, 'Schedule an appointment with S Supermarket', 1, 1),
-        (2, 'Attend marketing lecture at Y Stadium', 1, 2),
-        (3, 'Set-up a Snapchat account', 1, 3),
-        (4, 'Revamping our style guidelilnes', 2, 5),
-        (5, 'Sponsor social influencers', 2, 4),
-        (6, 'Rent a billboard and make start making art pieces for it', 2, 4),
-        (7, 'Set-up ad campaigns', 2, 5);
+        (101, 'Schedule an appointment with S Supermarket', 100, 1),
+        (102, 'Attend marketing lecture at Y Stadium', 100, 2),
+        (103, 'Set-up a Snapchat account', 100, 3),
+        (201, 'Revamping our style guidelilnes', 200, 5),
+        (202, 'Sponsor social influencers', 200, 4),
+        (203, 'Rent a billboard and make start making art pieces for it', 200, 4),
+        (204, 'Set-up ad campaigns', 200, 5);
 
 
 -- Subtasks
--- src/test/java/com/four/simple/repositories/TaskTest.java
+++ src/test/java/com/four/simple/repositories/TaskTest.java
@@ -11,9 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.four.simple.models.Task;
 
-
-
-
 /*
  * Test TaskRepository:
  * 
@@ -30,21 +27,27 @@ public class TaskTest {
     private TaskRepository taskRepo;
 
     @Test
-    public void testFindByListId()
+    public void testCRUD()
     {
-        List<Task> tasksL1 = taskRepo.findByListId(1L);
-        assertEquals(3, tasksL1.size());
+        long count = taskRepo.count();
 
-        List<Task> tasksL2 = taskRepo.findByListId(2L);
-        assertEquals(4, tasksL2.size());
+        // test CREATE
+        Task task = new Task();
+        task.setDescription("New task");
+        task.setId(30);
+        task.setListId(1L);
+        task.setStatusId(1L);
+        taskRepo.save(task);
 
-         // test READ
-         Task task = taskRepo.findById(1L).get();
-         assertEquals(true, task != null);
-         assertEquals(1L, task.getId());
-         assertEquals("Schedule an appointment with S Supermarket", task.getDescription());
+        assertEquals(count + 1, taskRepo.count());
 
-          // test UPDATE
+        // test READ
+        task = taskRepo.findById(101L).get();
+        assertEquals(true, task != null);
+        assertEquals(101L, task.getId());
+        assertEquals("Schedule an appointment with S Supermarket", task.getDescription());
+
+        // test UPDATE
         task = taskRepo.findById(1L).get();
         task.setDescription("Testing Task");
         taskRepo.save(task);
@@ -56,7 +59,16 @@ public class TaskTest {
         taskRepo.deleteById(1L);
         Optional<Task> deleted = taskRepo.findById(1L);
         assertEquals(false, deleted.isPresent());
+    }
 
+    @Test
+    public void testFindByListId()
+    {
+        List<Task> tasksL1 = taskRepo.findByListId(100L);
+        assertEquals(3, tasksL1.size());
+
+        List<Task> tasksL2 = taskRepo.findByListId(200L);
+        assertEquals(4, tasksL2.size());
     }
 }
 
