901fa6ffff3ca0e3dce130ee164d0fea270e3089
Satya Yoganand <satyaadd22@gmail.com>
2023-04-20 12:29:54 -0500

    Added CRUD Operations for Workspace Controller

--- src/main/java/com/four/simple/controllers/WorkspaceController.java
+++ src/main/java/com/four/simple/controllers/WorkspaceController.java
@@ -3,7 +3,11 @@ package com.four.simple.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +29,28 @@ public class WorkspaceController {
     @GetMapping
     public List<Workspace> getWorkspaces() {
         return workspaceRepository.findAll();
+
     }
+
+    @GetMapping("/{id}")
+    public Workspace getWorkspaceByID(@PathVariable long id){
+
+        return workspaceRepository.findById(id);
+    }
+
+    @PostMapping
+    public Workspace createWorkspace(@RequestBody Workspace workspace){
+        return workspaceRepository.save(workspace);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteWorkspace(@PathVariable long id){
+        workspaceRepository.deleteById(id);
+    }
+
+
+    
+
+
     
 }
