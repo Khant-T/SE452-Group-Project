33e318bae220d18acad449700e872b796f92a2a7
Satya Yoganand <satyaadd22@gmail.com>
2023-04-20 15:03:40 -0500

    Updated TaskController with CRUD Operations

--- src/main/java/com/four/simple/controllers/TaskController.java
+++ src/main/java/com/four/simple/controllers/TaskController.java
@@ -3,7 +3,11 @@ package com.four.simple.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,22 @@ public class TaskController {
     public List<Task> getTasks() {
         return taskRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Task getWorkspaceByID(@PathVariable long id){
+
+        return taskRepository.findById(id);
+    }
+
+    @PostMapping
+    public Task createWorkspace(@RequestBody Task task){
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteWorkspace(@PathVariable long id){
+        taskRepository.deleteById(id);
+    }
+
 }
+
