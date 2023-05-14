a272ace12e7bf6e90850d589fa45e4ab3b04230e
Khant Tayza <ktayza@depaul.edu>
2023-05-14 14:13:57 -0500

    Add test cases for Task

--- src/main/java/com/four/simple/task/Task.java
+++ src/main/java/com/four/simple/task/Task.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.four.simple.checklist.Checklist;
 import com.four.simple.checklist.Status;
 
-// import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +13,16 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table
 public class Task {
--- src/test/java/com/four/simple/repositories/TaskTest.java
+++ src/test/java/com/four/simple/repositories/TaskTest.java
@@ -1,80 +1,64 @@
-// package com.four.simple.repositories;
+package com.four.simple.repositories;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// import com.four.simple.task.Task;
-// import com.four.simple.task.TaskRepository;
+import com.four.simple.task.Task;
+import com.four.simple.task.TaskRepository;
 
-// /*
-//  * Test TaskRepository:
-//  * 
-//  *  - CREATE
-//  *  - READ
-//  *  - UPDATE
-//  *  - DELETE
-//  *  - findByListId
-//  */
+/*
+ * Test TaskRepository:
+ * 
+ *  - CREATE
+ *  - READ
+ *  - UPDATE
+ *  - DELETE
+ * 
+ * For later:
+ *  - findByListId
+ */
 
-// @SpringBootTest
-// public class TaskTest {
-//     @Autowired
-//     private TaskRepository taskRepo;
+ @SpringBootTest
+ public class TaskTest
+ {
+    @Autowired
+    private TaskRepository taskRepo;
 
-//     @Test
-//     public void testCRUD() {
-//         long count = taskRepo.count();
+    @Test
+    public void testCRUD()
+    {
+        long count = taskRepo.count();
 
-//         // test CREATE
-//         Task task = new Task();
-//         task.setDescription("New task");
-//         task.setId(30);
-//         task.setListId(1L);
-//         task.setStatusId(1L);
-//         taskRepo.save(task);
+        // Test CREATE
+        Task taskA = Task.builder().description("Task A").build();
+        Task taskB = Task.builder().description("Task B").build();
+        taskRepo.saveAll(List.of(taskA, taskB));
+        assertEquals(count + 2, taskRepo.count());
 
-//         assertEquals(count + 1, taskRepo.count());
+        // Get IDs of newly created Task Objects
+        long taskAId = taskA.getId();
+        long taskBId = taskB.getId();
 
-//         // test READ
-//         task = taskRepo.findById(401L);
-//         assertEquals(true, task != null);
-//         assertEquals(401L, task.getId());
-//         assertEquals("Schedule an appointment with S Supermarket", task.getDescription());
+        // Test READ
+        Task taskBRead = taskRepo.findById(taskBId);
+        assertEquals(true, taskBRead != null);
+        assertEquals("Task B", taskBRead.getDescription());
 
-//         // test UPDATE
-//         task = taskRepo.findById(401L);
-//         task.setDescription("Testing Task");
-//         taskRepo.save(task);
+        // Test UPDATE
+        Task taskBUpdate = taskBRead;
+        taskBUpdate.setDescription("Task B, Updated");
+        taskRepo.save(taskBUpdate);
 
-//         Task updated = taskRepo.findById(401L);
-//         assertEquals("Testing Task", updated.getDescription());
+        taskBUpdate = taskRepo.findById(taskBId);
+        assertEquals("Task B, Updated", taskBUpdate.getDescription());
 
-//         // test DELETE
-//         taskRepo.deleteById(401L);
-//         assertEquals(false, taskRepo.existsById(401L));
-//     }
-
-
-//     @Test
-//     public void testFindByListId() {
-//         List<Task> tasksL1 = taskRepo.findByListId(202L);
-//         assertEquals(4, tasksL1.size());
-
-//         List<Task> tasksL2 = taskRepo.findByListId(201L);
-//         assertEquals(3, tasksL2.size());
-//     }
-
-//     @Test
-//     public void testFindByStatusId() {
-//         List<Task> tasksS1 = taskRepo.findByStatusId(351L);
-//         assertEquals(2, tasksS1.size());
-
-//         List<Task> tasksS2 = taskRepo.findByStatusId(303L);
-//         assertEquals(1, tasksS2.size());
-//     }
-// }
+        // test DELETE
+        taskRepo.deleteById(taskAId);
+        assertEquals(false, taskRepo.existsById(taskAId));
+    }
+ }
