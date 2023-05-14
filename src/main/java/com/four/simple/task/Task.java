72bfdba9eb430935f74c6638fb667cb87096709a
Khant Tayza <ktayza@depaul.edu>
2023-05-13 21:22:51 -0500

    Trivial update

--- src/main/java/com/four/simple/task/Task.java
+++ src/main/java/com/four/simple/task/Task.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.four.simple.checklist.Checklist;
 import com.four.simple.checklist.Status;
 
-import jakarta.persistence.Column;
+// import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
