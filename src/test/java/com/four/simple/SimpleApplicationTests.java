87b00fceea8c96d186c3ca16542341361ab4599b
Satya Yoganand <satyaadd22@gmail.com>
2023-05-19 13:13:17 -0500

    "Commented Tests"

--- src/test/java/com/four/simple/SimpleApplicationTests.java
+++ src/test/java/com/four/simple/SimpleApplicationTests.java
@@ -1,14 +1,14 @@
-package com.four.simple;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SimpleApplicationTests {
-
-	@Test
-	void contextLoads() {
-		
-	}
-
-}
+//package com.four.simple;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SimpleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//}
