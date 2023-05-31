cc0f8ef32c15a11eb1cf7475aed00f33d0ffc087
Khant Tayza <ktayza@depaul.edu>
2023-05-31 11:22:44 -0500

    Add SubtaskController

---
+++ src/main/java/com/four/simple/task/SubtaskController.java
@@ -0,0 +1,52 @@
+package com.four.simple.task;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping(path = "subtasks")
+@Tag(name = "Subtask", description = "Subtask API")
+public class SubtaskController 
+{
+    @Autowired
+    private SubtaskRepository subtaskRepository;
+
+    @GetMapping
+    @Operation(summary = "Retrieve all available subtasks")
+    public List<Subtask> getSubtasks()
+    {
+        return subtaskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Retrieve a specific task based on the provided ID")
+    public Subtask getTasksByID(@PathVariable long id)
+    {
+        return subtaskRepository.findById(id);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new task")
+    public Subtask createTask(@RequestBody Subtask subtask)
+    {
+        return subtaskRepository.save(subtask);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a task based on the provided ID")
+    public void deleteTask(@PathVariable long id)
+    {
+        subtaskRepository.deleteById(id);
+    }
+}
