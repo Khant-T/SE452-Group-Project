e0b1333de04e585aba3580d107fe2b8bd1661bd7
neeha-github <131314874+neeha-github@users.noreply.github.com>
2023-06-04 21:34:04 -0400

    Create package-info.java

---
+++ src/main/java/com/four/simple/checklist/package-info.java
@@ -0,0 +1,22 @@
+package com.four.simple.checklist;
+
+/**
+ *
+ * <ul>
+ *      <li>contributed on checklist</li>
+ *      <li> contributed on checklist controller</li>
+ *      <li>contributed on status</li>
+ *      <li> contributed on StatusRepository</li>
+ *      <li>Added tests for the repositories</li>
+ *      <li>Added minimal changes to postgres and mongo db</li>
+ *      <li>Added StatusController</li>
+ *      <li>Added minimal documentation for Swagger-UI</li>
+ 
+ *      <li>Implemented minimals (views) UI for the status listings</li>
+ *      <li>Implemented status service to render the views</li>
+ * </ul>
+ *
+ * @author: neeha kalluri
+ *
+ *
+ */
