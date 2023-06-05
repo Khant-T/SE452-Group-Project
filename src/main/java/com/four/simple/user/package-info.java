b83ef60c311cfac0899961d95f04fcf07370c276
Xuwei Lin <lxw824178965@gmail.com>
2023-06-04 21:11:42 -0500

    Update package-info.java
    Adding more on package-info.

--- src/main/java/com/four/simple/user/package-info.java
+++ src/main/java/com/four/simple/user/package-info.java
@@ -7,11 +7,12 @@
  *      <li>Grand access to different role of users</li>
  *      <li>Implemented RegistrationService to render the views</li>
  *      <li>Implemented minimals (views) UserInterface for Registration Page</li>
-
+ *      <li>Please remember the user have to register before viewing the page. And PLEASE DO NOT REGISTER </li>
+ *      <li>AS AN ADMIN/USER/USERA/USERB/USERC/USERD/USERF/USERE || IF YOU HAVE DIFFERENT ROLE, YOU CAN LOGIN AGAIN TO GRANT ACCESS</li>
  * </ul>
  *
  * @author: Xuwei Lin
- * @HelpBy: Professor KenYu, Hal Tayza
+ * @HelpBy: Professor KenYu, Hal Tayza, Satya Yoganand Addala
  *
  * @note  : Every team member contributed with debugging, testings, and suggesting ideas. Inspired by Professor KenYu's Security Demo.
  *
